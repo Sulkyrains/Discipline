b286df9684ccf50e9a8834aaac3bd039962f0351
Sulkyrains <sulkyrains@gmail.com>
2026-08-07 14:59:21 +0800

    Updates

---
+++ assets/index-Jxnf2FaI.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-vchfzPMi.js","assets/index-BYFok-B_.js","assets/index-Bvc0wX0e.css"])))=>i.map(i=>d[i]);
+import{s as p,_ as r}from"./index-BYFok-B_.js";const _=p("App",{web:()=>r(()=>import("./web-vchfzPMi.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
