f288cc2f603dcdbdce61ae6df3614052f346e9c3
Sulkyrains <sulkyrains@gmail.com>
2026-08-07 15:45:47 +0800

    Updates

--- assets/index-BnjLyDom.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BImGVe3-.js","assets/index-CAOam8xe.js","assets/index-D369wgfO.css"])))=>i.map(i=>d[i]);
-import{s as p,_ as r}from"./index-CAOam8xe.js";const _=p("App",{web:()=>r(()=>import("./web-BImGVe3-.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
