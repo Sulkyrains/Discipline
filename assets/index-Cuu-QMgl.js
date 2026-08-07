ca7c123954ad48a684a5c9feb54ef632e3367190
Sulkyrains <sulkyrains@gmail.com>
2026-08-07 23:49:13 +0800

    Updates

---
+++ assets/index-Cuu-QMgl.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-COo67boK.js","assets/index-BZAqIC5X.js","assets/index-ChjYrrKb.css"])))=>i.map(i=>d[i]);
+import{x as e,_ as p}from"./index-BZAqIC5X.js";const _=e("App",{web:()=>p(()=>import("./web-COo67boK.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
