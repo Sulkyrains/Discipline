278588b2fd97ae8068807e256685c2a3b872da77
Sulkyrains <sulkyrains@gmail.com>
2026-08-07 19:29:42 +0800

    Updates

---
+++ assets/index-D8kzWG-m.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CrsVWWU7.js","assets/index-vZdPy50d.js","assets/index-DFEeVK4-.css"])))=>i.map(i=>d[i]);
+import{v as p,_ as r}from"./index-vZdPy50d.js";const _=p("App",{web:()=>r(()=>import("./web-CrsVWWU7.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
--- assets/index-iui3Eshr.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CrEZRrFP.js","assets/index-lfYKw4xs.js","assets/index-CjPjITV_.css"])))=>i.map(i=>d[i]);
-import{s as p,_ as r}from"./index-lfYKw4xs.js";const _=p("App",{web:()=>r(()=>import("./web-CrEZRrFP.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
