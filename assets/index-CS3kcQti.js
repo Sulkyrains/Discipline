59666a8b26f36f1cb753f7f686d71f9a75cf7d93
Sulkyrains <sulkyrains@gmail.com>
2026-08-07 21:40:22 +0800

    Updates

--- assets/index-CS3kcQti.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Ptv97ir_.js","assets/index-q-Dbijxf.js","assets/index-BYsphodm.css"])))=>i.map(i=>d[i]);
-import{v as p,_ as r}from"./index-q-Dbijxf.js";const _=p("App",{web:()=>r(()=>import("./web-Ptv97ir_.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
