866b0292578104e4a2f40dd60f98c72b1f1c2929
Sulkyrains <sulkyrains@gmail.com>
2026-08-07 23:25:38 +0800

    Updates

--- assets/index-BCf0LAX8.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-HIBNfKfB.js","assets/index-D3XA-f5h.js","assets/index-ChjYrrKb.css"])))=>i.map(i=>d[i]);
-import{x as e,_ as p}from"./index-D3XA-f5h.js";const _=e("App",{web:()=>p(()=>import("./web-HIBNfKfB.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
---
+++ assets/index-CfvLsGe4.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-C4QVkY_q.js","assets/index-DqxMw6Zb.js","assets/index-ChjYrrKb.css"])))=>i.map(i=>d[i]);
+import{x as e,_ as p}from"./index-DqxMw6Zb.js";const _=e("App",{web:()=>p(()=>import("./web-C4QVkY_q.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
