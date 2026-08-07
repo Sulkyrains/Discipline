4899557b38b063ca110af39577d3b6a5b04b5998
Sulkyrains <sulkyrains@gmail.com>
2026-08-07 20:32:32 +0800

    Updates

--- assets/index-D64kJAri.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DRqA_nT7.js","assets/index-Db_RbufU.js","assets/index-CplG7-Au.css"])))=>i.map(i=>d[i]);
-import{v as p,_ as r}from"./index-Db_RbufU.js";const _=p("App",{web:()=>r(()=>import("./web-DRqA_nT7.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
