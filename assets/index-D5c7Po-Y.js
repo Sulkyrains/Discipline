6d0d565fe262f44b0ef66b62319fb226fccaaa73
Sulkyrains <sulkyrains@gmail.com>
2026-08-07 23:56:10 +0800

    Updates

---
+++ assets/index-D5c7Po-Y.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-EkujBgjE.js","./index-C5wxL434.js","./index-ChjYrrKb.css"])))=>i.map(i=>d[i]);
+import{x as e,_ as p}from"./index-C5wxL434.js";const t=e("App",{web:()=>p(()=>import("./web-EkujBgjE.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.AppWeb)});export{t as App};
--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Discipline/sw.js', { scope: '/Discipline/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
