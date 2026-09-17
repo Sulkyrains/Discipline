5ecb9eac289b6f740069c3cfbf5c03578b09a882
Sulkyrains <sulkyrains@gmail.com>
2026-09-17 23:09:07 +0800

    Updates

---
+++ assets/index-BGI7p-tA.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CVR2Lsrq.js","./index-Dq_3a_PU.js","./index-BOFiwyBC.css"])))=>i.map(i=>d[i]);
+import{a9 as e,aa as p}from"./index-Dq_3a_PU.js";const t=e("App",{web:()=>p(()=>import("./web-CVR2Lsrq.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.AppWeb)});export{t as App};
